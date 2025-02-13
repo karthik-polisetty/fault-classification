4ce86d69d8966967c5f7e8a94dfdcef474a0f10f
Krishna Karthik Polisetty <kpolisetty1524@gmail.com>
2025-02-12 19:36:06 -0500

    modified app.py

--- app.py
+++ app.py
@@ -117,4 +117,9 @@ async def upload_page(request: Request):
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, port=8000)
+    uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
+
+
+
+
+
